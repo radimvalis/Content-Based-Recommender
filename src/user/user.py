e03c03a13abdb3cd11d9b5f5728afdead6f317ac
Radim Vališ <radim.valis3@gmail.com>
2023-04-26 16:33:31 +0200

    Implement representation of user

--- src/user/user.py
+++ src/user/user.py
@@ -1,3 +1,26 @@
+from storage import StorageItem
 
 class UserProfile:
-    pass
+
+    def __init__(self, username: str, view_history: list[StorageItem] = [], max_view_history_len: int = 5) -> None :
+        self.__username = username
+        self.__view_history = view_history
+        self.__max_view_history_len = max_view_history_len
+
+    def username(self) -> str :
+        return self.__username
+
+    def preferences(self) -> set[str] :
+        preferences = set()
+        
+        for item in self.__view_history:
+            for keyword in item.keywords():
+                preferences.add(keyword)
+
+        return preferences
+    
+    def update_view_history(self, viewed_item: StorageItem) -> None :
+        if len(self.__view_history) >= self.__max_view_history_len:
+            self.__view_history.pop(0)
+
+        self.__view_history.append(viewed_item)
