9f02e2b5a9fbd75848c1ae35712e7eb6a9318f5a
Radim Vališ <radim.valis3@gmail.com>
2023-04-28 14:54:05 +0200

    Implement Recommender

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -4,10 +4,23 @@ from storage import Storage, StorageItem
 
 class Recommender:
 
-    __storage: list[StorageItem] = []
+    def __init__(self) -> None:
+        self.__storage: list[StorageItem] = []
 
-    def recommend(self, user_profile: UserProfile) -> list[StorageItem] :
-        pass
+    def recommend_to(self, user_profile: UserProfile, max_recommendations_count: int = 100) -> list[StorageItem]:
+        user_preferences = user_profile.preferences()
+        self.__storage.sort(
+            key = lambda item: Recommender.__jaccard_index(item.keywords(), user_preferences), reverse = True
+        )
 
-    def load_storage(self, storage: Storage) -> None :
+        if len(self.__storage) < max_recommendations_count:
+            return self.__storage.copy()
+
+        return self.__storage[:max_recommendations_count]
+
+    def load_storage(self, storage: Storage) -> None:
         self.__storage = storage.get_data()
+
+    def __jaccard_index(a: set[str], b: set[str]) -> float:
+        intersection_size = len(a.intersection(b))
+        return intersection_size / (len(a) + len(b) - intersection_size)
