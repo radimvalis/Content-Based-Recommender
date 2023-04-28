878ee284f89ec8504ddde6c0a99109c94f3882b7
Radim Vališ <radim.valis3@gmail.com>
2023-04-28 13:43:14 +0200

    Change init of history list in UserProfile

--- src/user/user.py
+++ src/user/user.py
@@ -2,9 +2,9 @@ from storage import StorageItem
 
 class UserProfile:
 
-    def __init__(self, username: str, view_history: list[StorageItem] = [], max_view_history_len: int = 5) -> None :
+    def __init__(self, username: str, view_history: list[StorageItem] = None, max_view_history_len: int = 5) -> None:
         self.__username = username
-        self.__view_history = view_history
+        self.__view_history = view_history if view_history is not None else []
         self.__max_view_history_len = max_view_history_len
 
     def username(self) -> str :
@@ -19,7 +19,8 @@ class UserProfile:
 
         return preferences
     
-    def update_view_history(self, viewed_item: StorageItem) -> None :
+    def update_view_history(self, viewed_item: StorageItem) -> None:
+
         if len(self.__view_history) >= self.__max_view_history_len:
             self.__view_history.pop(0)
 
